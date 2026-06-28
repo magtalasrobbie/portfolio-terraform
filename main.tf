63319c9d9614ac5e75a69d64b20a4217496990a1
magtalasrobbie <magtalasrobbie@gmail.com>
2026-06-27 20:58:38 -0700

    Add S3 + CloudFront portfolio infrastructure via Terraform

---
+++ main.tf
@@ -0,0 +1,98 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"}
+    }
+}
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "portfolio" {
+    bucket = "robbiemagtalas-portfolio-tf"
+}
+
+resource "aws_s3_bucket_website_configuration" "portfolio" {
+    bucket = aws_s3_bucket.portfolio.id
+
+    index_document {
+        suffix = "index.html"
+    }
+
+    error_document {
+        key = "error.html"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "portfolio" {
+    bucket = aws_s3_bucket.portfolio.id
+
+    block_public_acls       = false
+    block_public_policy     = false
+    ignore_public_acls      = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "portfolio" {
+    bucket = aws_s3_bucket.portfolio.id
+    depends_on = [ aws_s3_bucket_public_access_block.portfolio ]
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Sid     = "PublicReadGetObject"
+                Effect  = "Allow"
+                Principal = "*"
+                Action = "s3:GetObject"
+                Resource = "${aws_s3_bucket.portfolio.arn}/*"
+            }
+        ]
+    })
+}
+
+resource "aws_cloudfront_distribution" "portfolio" {
+    origin {
+      domain_name = aws_s3_bucket_website_configuration.portfolio.website_endpoint
+      origin_id = "portfolioS3Origin"
+
+      custom_origin_config {
+        http_port = 80
+        https_port = 443
+        origin_protocol_policy = "http-only"
+        origin_ssl_protocols = [ "TLSv1.2" ]
+      }
+    }
+
+    enabled             = true
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+      allowed_methods  = ["GET", "HEAD"]
+      cached_methods   = ["GET", "HEAD"]
+      target_origin_id = "portfolioS3Origin"
+      viewer_protocol_policy = "redirect-to-https"
+
+      forwarded_values {
+        query_string = false
+        cookies {
+          forward = "none"
+        }
+      }
+    }
+
+      restrictions {
+        geo_restriction {
+          restriction_type = "none"
+        }
+      }
+      
+      viewer_certificate {
+        cloudfront_default_certificate = true
+      }
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.portfolio.domain_name
+}
